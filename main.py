3dc6b67a51053ab7fc4f917e578771ff3be9789e
Rachit-R <141769266+Rachit-R@users.noreply.github.com>
2024-06-20 11:57:27 +0100

    run_all_queries_generate_CSV
    Run SQL queries within python on SQL database and generate CSV

---
+++ main.py
@@ -0,0 +1,18 @@
+import sqlite3
+from queries import run_all_queries
+
+def main():
+    # Path to  SQLite database
+    db_path = 'isrctn_data.db'
+
+    # Connect to  SQLite database
+    conn = sqlite3.connect(db_path)
+
+    # Run all  queries and save  results to CSV files
+    run_all_queries(conn)
+
+    # Close  database connection
+    conn.close()
+
+if __name__ == "__main__":
+    main()
