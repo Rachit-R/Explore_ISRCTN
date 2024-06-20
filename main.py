2fe5a62ddfb07808c361c92238cd22ed07dbef0f
Rachit-R <141769266+Rachit-R@users.noreply.github.com>
2024-06-20 12:01:03 +0100

    Main_
    Updated formatting and ran queries

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import sqlite3
 from queries import run_all_queries
 
 def main():
-    # Path to  SQLite database
+    # Path to SQLite database
     db_path = 'isrctn_data.db'
 
     # Connect to  SQLite database
     conn = sqlite3.connect(db_path)
 
-    # Run all  queries and save  results to CSV files
+    # Run all queries; save results to CSV files
     run_all_queries(conn)
 
-    # Close  database connection
+    # Close database connection
     conn.close()
 
 if __name__ == "__main__":
